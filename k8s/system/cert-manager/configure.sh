e89217d5e58b8739c8366ef68b2189c04fc7f73c
ナッティ <hello@nutty.cc>
2019-08-08 22:12:36 -0700

    Edit issuer manifests in place

--- k8s/system/cert-manager/configure.sh
+++ k8s/system/cert-manager/configure.sh
@@ -1,5 +1,6 @@
 #/usr/bin/env bash
 
+DIR=$(dirname "$0")
 KEY=spec.acme.solvers.0.dns01.digitalocean.tokenSecretRef.key
-yq w staging-issuer.yaml ${KEY} ${1}
-yq w production-issuer.yaml ${KEY} ${1}
+yq w -i ${DIR}/staging-issuer.yaml ${KEY} ${1}
+yq w -i ${DIR}/production-issuer.yaml ${KEY} ${1}
