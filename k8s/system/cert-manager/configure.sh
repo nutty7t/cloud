cf5cb3e9e41bfe703b32e0298422f7aa395fe07a
ナッティ <hello@nutty.cc>
2019-08-08 22:17:46 -0700

    Create DNS solver secret

--- k8s/system/cert-manager/configure.sh
+++ k8s/system/cert-manager/configure.sh
@@ -1,6 +1,5 @@
 #/usr/bin/env bash
 
 DIR=$(dirname "$0")
-KEY=spec.acme.solvers.0.dns01.digitalocean.tokenSecretRef.key
-yq w -i ${DIR}/staging-issuer.yaml ${KEY} ${1}
-yq w -i ${DIR}/production-issuer.yaml ${KEY} ${1}
+KEY=data.access-token
+yq w -i ${DIR}/solver-secret ${KEY} ${1}
