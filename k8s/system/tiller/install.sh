444bbd06ca8e50e083861aaf803e37d00e73a3e8
ナッティ <hello@nutty.cc>
2020-02-23 12:15:13 -0700

    Remove tiller configuration
    No need for it anymore! Removed in Helm 3.

--- k8s/system/tiller/install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-kubectl create serviceaccount tiller --namespace=nutty-system
-kubectl create clusterrolebinding tiller-admin --serviceaccount=nutty-system:tiller --clusterrole=cluster-admin
-helm init --service-account=tiller --tiller-namespace=nutty-system
-
--- k8s/system/tiller/uninstall.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-helm reset
-kubectl delete deployment tiller-deploy --namespace=nutty-system
-kubectl delete service tiller-deploy --namespace=nutty-system
-kubectl delete serviceaccount tiller --namespace=nutty-system
-kubectl delete clusterrolebinding tiller-admin
-
