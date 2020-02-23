44bd8303fa0159b2012c903f91ef2f39c218c937
ナッティ <hello@nutty.cc>
2020-02-23 12:44:35 -0700

    Fix `access-token` base64 encoder

--- k8s/system/cert-manager/configure.sh
+++ k8s/system/cert-manager/configure.sh
@@ -1,5 +1,12 @@
 #/usr/bin/env bash
 
+# We might not need this.
+# We can use the b64enc template function in Helm.
+# Yeah, let's do that instead.
+# We're going to refactor these manifest files into a Helm chart.
+# access-token can be a property in the values.yaml file.
+
 DIR=$(dirname "$0")
 KEY=data.access-token
-yq w -i ${DIR}/solver-secret.yaml ${KEY} $(echo "${1}" | base64 -w 0)
+TOKEN=$(yq r ${DIR}/solver-secret.yaml ${KEY} | base64 -w 0)
+yq w -i ${DIR}/solver-secret.yaml ${KEY} ${TOKEN}
